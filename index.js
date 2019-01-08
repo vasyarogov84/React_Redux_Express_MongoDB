fa9a62e4399b08ffb4185187e6cf9521323a0af6
vasyarogov84 <viktorgrom84@gmail.com>
2019-01-07 17:34:21 -0800

    break down project, add passport.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
+ require("./services/passport");
+ require("./routes/authRoutes");
+
 const app = express();
 
-app.get('/', (req,res) => {
-  
-  res.send({ hi: 'there '});
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
