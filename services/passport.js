5cf7f10cb5d3706c955b29790ee8ccf7867a4ea7
vasyarogov84 <viktorgrom84@gmail.com>
2019-01-11 12:18:33 -0800

    add client side of app

--- services/passport.js
+++ services/passport.js
@@ -23,21 +23,19 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({
+    async (accessToken, refreshToken, profile, done) => {
+      let existingUser = await User.findOne({
         googleId: profile.id
-      }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            displayName: profile.displayName
-          })
-            .save()
-            .then(user => done(null, user));
-        }
       });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({
+        googleId: profile.id,
+        displayName: profile.displayName
+      }).save();
+      done(null, user);
     }
   )
 );
