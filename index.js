a9df13af4668e0e01b7dbe07ff94309835c9a72e
vasyarogov84 <viktorgrom84@gmail.com>
2019-01-16 15:02:04 -0800

    fix index.js for production

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/bilingRoutes")(app);
 
-if (process.env.PORT === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   const path = require('path');
